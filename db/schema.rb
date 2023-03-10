bdf14db5c7d8d181990d6aaf820301cd88f6053a
francois vallat <vallatfrancois@yahoo.fr>
2023-03-10 12:38:49 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_111439) do
     t.decimal "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["overview"], name: "index_movies_on_overview", unique: true
+    t.index ["title"], name: "index_movies_on_title", unique: true
   end
 
   add_foreign_key "bookmarks", "lists"
